ecf3d77d70a5228f47d2daf653303b35594968b1
Kalu <orjikalukelvin@gmail.com>
2026-01-26 20:33:17 -0500

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
+const local = 'http://localhost:8000/api';
+const dev = 'https://math-paste-api.onrender.com/api';
 
-
-const url = 'https://math-paste-api.onrender.com/api';
+const url = dev; // Switch between 'local' and 'dev' as needed
 const api = url + '/';
 export const environment = {
   production: false,
