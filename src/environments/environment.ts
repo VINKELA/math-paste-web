d0b9d1b2e431f3ce3b1a9f20b618d1484bef625d
Kalu <orjikalukelvin@gmail.com>
2026-03-17 15:15:46 -0400

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 const local = 'http://localhost:8000/api';
-const dev = 'https://math-paste-api.onrender.com/api';
+const dev = 'https://api.formulaclipboard.io/api';
 
 const url = dev; // Switch between 'local' and 'dev' as needed
 const api = url + '/';
